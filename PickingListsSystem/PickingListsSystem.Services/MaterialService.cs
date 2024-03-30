70cdc34acc603edeec1bf9badc2971457d33ce4a
Karabas18 <nikitarafa2232@gmail.com>
2024-03-30 16:43:30 +0300

    Add services

--- PickingListsSystem/PickingListsSystem.Services/MaterialService.cs
+++ PickingListsSystem/PickingListsSystem.Services/MaterialService.cs
@@ -1,9 +1,45 @@
-﻿using PickingListsSystem.Services.Contracts;
+﻿using AutoMapper;
+using PickingListsSystem.DataAccess.Contracts;
+using PickingListsSystem.Dto.Materials;
+using PickingListsSystem.Services.Contracts;
 
 namespace PickingListsSystem.Services
 {
     public class MaterialService : IMaterialService
     {
+        private readonly IMapper _mapper;
+        private readonly IMaterialRepository _materialRepository;
 
+        public MaterialService(IMapper mapper, IMaterialRepository materialRepository)
+        {
+            _mapper = mapper;
+            _materialRepository = materialRepository;
+        }
+
+        public Task<int> AddMaterial(CreateMaterialDto material)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMaterial(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MaterialDto> GetMaterialID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<MaterialDto>> GetMaterials() 
+        {
+            var  materials = await _materialRepository.GetMaterials();
+            return _mapper.Map<List<MaterialDto>>(materials);
+        }
+
+        public Task<int> UpdateMaterial(MaterialDto material)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ PickingListsSystem/PickingListsSystem.Web/Profiles/MaterialProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PickingListsSystem.Dto.Materials;
+using PickingListsSystem.Entities;
+
+namespace PickingListsSystem.API.Profiles
+{
+    public class MaterialProfile : Profile
+    {
+        public MaterialProfile()
+        {
+            CreateMap<Material, MaterialDto>().ReverseMap();
+        }
+    }
+}
